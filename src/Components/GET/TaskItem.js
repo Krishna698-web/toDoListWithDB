13dc02da0192e202228427df962d0eb6799e9b5d
Krishan <kmkrider1999@gmail.com>
2023-06-06 11:55:34 +0530

    Updated index.css

--- src/Components/GET/TaskItem.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./DisplayTasks.module.css";
-
-const TaskItem = ({ taskText }) => {
-  return <div className={styles.task}>{taskText}</div>;
-};
-
-export default TaskItem;
